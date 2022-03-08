14b8490cd0c026d1b7af62bf89859ebf0a47a167
ivannagwen <ivanna07@uw.edu>
2022-03-08 00:18:39 -0800

    delete scripts folder, merging all to one ui file

--- scripts/rank_comparison_server.R
+++
@@ -1,6 +0,0 @@
-# Chart 1 
-# Where does COVID-19 rank related to other long-term causes grouped by specific factors such as location, age group, gender?
-
-server <- function(input, output) {
-  
-}
